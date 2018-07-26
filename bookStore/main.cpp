416f40061f45b7f512265b91039b315913de422f
amit levy <amit.p.levy@gmail.com>
2018-07-26 21:28:34 +0300

    Initial Commit

---
+++ bookStore/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bookStore
+//
+//  Created by Amit Levy on 26/07/2018.
+//  Copyright © 2018 Amit Levy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
